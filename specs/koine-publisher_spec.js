1cb7e9a7977b1e8d93357419f848616845270fee
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-09-30 14:42:56 -0300

    Subscription ready

--- specs/koine-publisher_spec.js
+++ specs/koine-publisher_spec.js
@@ -3,6 +3,74 @@ if (typeof(require) === 'function') {
 }
 
 describe("Koine.Publisher", function () {
+    var publisher;
+
     beforeEach(function () {
+        publisher = new Koine.Publisher();
+    });
+
+    it("can be instanciated", function () {
+        expect(publisher).toBeDefined();
+    });
+
+    it("publishes callbacks", function () {
+        // prepare
+        var output = null;
+
+        publisher.subscribe('event', function (data) {
+            output = 'called with: ' + data.vars.join(', ');
+        });
+
+        // executes
+        publisher.publish('event', { vars: ['a', 'b', 'c']});
+
+        // verifies
+        expect(output).toEqual('called with: a, b, c');
+    });
+
+    it("can clear subscriptions", function () {
+        var output = '';
+
+        publisher.subscribe('event', function () {
+            output = 'a';
+        });
+
+        publisher.clearSubscriptions();
+
+        publisher.publish('event');
+
+        expect(output).toEqual('');
+    });
+
+    describe('.unsubscribe()', function () {
+        var a, b, c;
+
+        beforeEach(function () {
+            output = '';
+
+            a = function () { output += 'a'; };
+            b = function () { output += 'b'; };
+            c = function () { output += 'c'; };
+
+            publisher.subscribe('event', a);
+            publisher.subscribe('event', b);
+            publisher.subscribe('event', c);
+            publisher.subscribe('event', b);
+        });
+
+        it("it can unsubscribe single event", function () {
+            publisher.unpublish('event', b);
+
+            publisher.publish('event');
+            expect(output).toEqual('ac');
+        });
+
+        it("it can unsubscribe all callbacks from an event", function () {
+            publisher.unpublish('event');
+
+            publisher.publish('event');
+
+            expect(output).toEqual('');
+        });
     });
 });
--- src/koine-publisher.js
+++ src/koine-publisher.js
@@ -1,6 +1,49 @@
 (function (Koine) {
     "use strinct";
 
-    Koine.Publisher = function () {};
+    Koine.Publisher = function () {
+        this.clearSubscriptions();
+    };
+
+    Koine.Publisher.prototype.subscribe = function (type, callback) {
+        this._subscriptions[type] = this._subscriptions[type] || [];
+        this._subscriptions[type].push(callback);
+
+        return this;
+    };
+
+    Koine.Publisher.prototype.publish = function (type, data) {
+        var subscriptions = this._subscriptions[type] || [];
+
+        subscriptions.forEach(function (callback) {
+            callback(data);
+        });
+
+        return this;
+    };
+
+
+    Koine.Publisher.prototype.clearSubscriptions = function () {
+        this._subscriptions = {};
+    };
+
+    Koine.Publisher.prototype.unpublish = function (type, callback) {
+        if (callback) {
+            var callbacks = this._subscriptions[type] || [];
+            var index = 1;
+
+            while (index >= 1) {
+                index = callbacks.indexOf(callback);
+
+                if (index >= 0) {
+                    callbacks.splice(index, 1);
+                }
+            }
+        } else if(type) {
+            this._subscriptions[type] = [];
+        }
+
+        return this;
+    };
 
 })(typeof(exports) === "undefined" ? (this.Koine || (this.Koine = {})) : exports);
